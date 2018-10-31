5d8c294755110d8b2a2e3a8b42d4750ba2141bea
user <user@192.168.11.225>
2018-10-31 15:57:03 +0900

    JP_school

---
+++ src/Test1.java
@@ -0,0 +1,38 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Test1 {
+	private int[] list;
+	private static final int SIZE = 10;
+	
+	public Test1() {
+		list = new int[SIZE];
+		for (int i = 0; i < SIZE; i++)
+			list[i] = i;
+		writeList();
+	}
+	
+	public void writeList() {
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(new FileWriter("outfile.txt"));
+			for(int i = 0; i < SIZE; i++)
+				out.println("배열 원소" + i + "=" + list[i]);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.err.println("ArrayIndexOutOfBoundsException: ");
+		}catch(IOException e) {
+			System.err.println("IOException");
+		}finally {
+			if(out != null)
+				out.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Test1();
+	}
+
+}
+
+
